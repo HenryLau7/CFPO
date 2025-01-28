41c5d77cfb0a898216304a2417ad2e8354428d33
yuanyeliu7@gmail.com <yuanyeliu7@gmail.com>
2025-01-27 22:42:27 -0800

    some init files

--- src/init_prompts/__init__.py
+++ src/init_prompts/__init__.py
@@ -1,6 +1,6 @@
 """
-__paper__ = PODE
+__paper__ = Beyond Prompt Content: Enhancing LLM Performance via Content-Format Integrated Prompt Optimization (CFPO)
 __author__ = Yuanye Liu#, Jiahang Xu#
 __version__ = 1.0
-__date__ = 2025-1-13
+__date__ = 2025-1
 """
